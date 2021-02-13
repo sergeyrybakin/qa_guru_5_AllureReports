444781b198ce45841452ae0de2a8061ad1156d65
Sergey Rybakin <sergey.rybakin@gmail.com>
2021-02-13 12:49:50 +0700

    Selenide simple test search issue on GitHub. is working properly.

--- src/test/java/tests/GitHubIssueSearchSelenide.java
+++ src/test/java/tests/GitHubIssueSearchSelenide.java
@@ -5,12 +5,16 @@ import org.openqa.selenium.By;
 
 import com.codeborne.selenide.Selenide;
 
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class GitHubIssueSearchSelenide extends TestBase {
     private static final String BASE_URL = "https://github.com";
     private static final String REPOSITORY = "selenide/selenide";
-    private static final String SEARCH_CRITERIA = "sergeyrybakin";
+    private static final String SEARCH_CRITERIA = "is:issue sergeyrybakin";
     private static final String EXPECTED_RESULT = "Feature request: add Condition attributeContainsValue";
 
     @Test
@@ -22,5 +26,15 @@ public class GitHubIssueSearchSelenide extends TestBase {
 
         //Open repository
         $(By.linkText(REPOSITORY)).click();
+
+        //Click on Issues in top menu
+        $(withText("Issues")).click();
+
+        //Find my request
+        $("#js-issues-search").clear();
+        $("#js-issues-search").val(SEARCH_CRITERIA).submit();
+
+        //Expected result:
+        $$("div.js-navigation-container div").findBy(text(EXPECTED_RESULT)).shouldBe(appear);
     }
 }
